dd748073edb49e8e1d5e82456a0aeda4fefdaebe
Rajdeep Rao <rajdeep.rao@datadoghq.com>
2022-12-17 14:31:10 -0500

    Add cloudfront and s3 infra

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,3 +7,12 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_s3_bucket" "tf_backend" {
+    bucket = "time-capsule-terraform-backend"
+}
+
+resource "aws_s3_bucket_acl" "tf_backend_acl" {
+  bucket = aws_s3_bucket.tf_backend.id
+  acl    = "private"
+}
