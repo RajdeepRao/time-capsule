bd85e0757821f9b7cd0c5c7bdb7ab53adec8920c
Rajdeep Rao <rajdeep.rao@datadoghq.com>
2022-12-26 17:17:33 -0500

    Mask env vars

--- src/flask/authenticate.py
+++ src/flask/authenticate.py
@@ -1,21 +1,11 @@
 import pyrebase
-
-config = {
-  "apiKey": "AIzaSyDzJaFxs8pg_r7-3AEf-JH6CuBv1Y83vGk",
-  "authDomain": "time-capsule-66dc4.firebaseapp.com",
-  "projectId": "time-capsule-66dc4",
-  "storageBucket": "time-capsule-66dc4.appspot.com",
-  "messagingSenderId": "519401308847",
-  "appId": "1:519401308847:web:7a5a580d5396d59780ca5a",
-  "measurementId": "G-52M0T7G4HN",
-  "databaseURL": ""
-}
+from firebase_config import config
 
 def auth():
     firebase = pyrebase.initialize_app(config)
     auth = firebase.auth()
 
-    email = 'test@123.com'
+    email = 'test@1234.com'
     password = 'password'
 
     user = auth.create_user_with_email_and_password(email, password)
