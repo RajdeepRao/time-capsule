f9c391b769c8c25aab935c92bf99ee230c650221
Rajdeep Rao <rajdeep.rao@datadoghq.com>
2022-12-26 17:19:35 -0500

    PoC nextjs

---
+++ nextjs/nextjs-blog/pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import Head from 'next/head';
+import Link from 'next/link'
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+    return (
+    <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <Image 
+        src="/images/profile.jpg"
+        alt="Your Name"
+        height={144} 
+        width={144}
+        />
+        <h2>
+          <Link href="/">← Back to home</Link>
+        </h2>
+      </Layout>
+    );
+  }
