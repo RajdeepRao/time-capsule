75d6dd398fe90c38b7f5cc86f97f178882231c6f
Rajdeep Rao <rajdeep.rao@datadoghq.com>
2023-02-11 12:59:28 -0500

    Enable live loading

--- src/flask/build-and-run.sh
+++ src/flask/build-and-run.sh
@@ -5,4 +5,4 @@ echo $status
 docker build . -t time-capsule:latest
 status="Running container on port 5000....."
 echo $status
-docker run --name local-time-capsule -p 5000:5000 -d time-capsule:latest
+docker run --name local-time-capsule -v `pwd`:/app -p 5000:5000 -d time-capsule:latest 
