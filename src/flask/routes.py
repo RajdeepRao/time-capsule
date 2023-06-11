d5941110038261c7859e620756724ea1fff26b0e
Rajdeep Rao <rajdeep.rao@datadoghq.com>
2023-06-11 19:47:47 -0400

    Add code for log in

--- src/flask/routes.py
+++ src/flask/routes.py
@@ -1,31 +1,76 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+# Std lib
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from functools import wraps
+import ipdb
+import json
 import os
-from utils.firebase import TimeCapsuleFirebaseObj
+
+# 3rd party
+import requests
+from requests.exceptions import HTTPError
+
+# local
+from utils.firebase_admin import TimeCapsuleFirebaseAdminObj
+from utils.pyrebase import TimeCapsulePyrebaseObj
+
 
 app = Flask(__name__)
 # Todo: Come back and env var this
 app.secret_key = "super secret key"
 # Init firebase obj
-time_capsule_firebase_obj = TimeCapsuleFirebaseObj()
+time_capsule_firebase_admin_obj = TimeCapsuleFirebaseAdminObj()
+time_capsule_pyrebase_obj = TimeCapsulePyrebaseObj()
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            if session['logged_in']:
+                print('checking login required...confirmed...redirecting')
+                return f(*args, **kwargs)
+        else:
+            flash("You need to login first", category='error')
+            return redirect(url_for('login'))
+    return wrap
 
 users = [{'uid': 1, 'name': 'Noah Schairer'}]
 @app.route('/api/userinfo')
+@login_required
 def userinfo():
     return {'data': users}, 200
 
 @app.route('/', methods=['GET'])
+@login_required
 def home():
     return render_template('index.html')
 
 @app.route('/auth-test', methods=['GET'])
 def authenticate_test():
-    auth()
     return {'status': 'ok'}, 200
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    email = request.form.get('email')
-    password = request.form.get('password')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print(f"{email} in the endpoint")
+        try:
+            response = time_capsule_pyrebase_obj.sign_user_in(email, password)
+        except HTTPError as e:
+            error_json = e.args[1]
+            error_code = json.loads(error_json)['error']['code']
+            error_message = json.loads(error_json)['error']['message']
+            flash(f'{error_message}', category='error')
+            return render_template('login.html')
+        #ipdb.set_trace()
+        session['logged_in'] = True
+        session['email_id'] = email
+        return redirect(url_for('home'))
+    
+    if 'logged_in' in session:
+            if session['logged_in']:
+                return redirect(url_for('home'))
+    
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -36,7 +81,7 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         # Query firebase to see if user exists
-        user = time_capsule_firebase_obj.get_user(email)
+        user = time_capsule_firebase_admin_obj.get_user(email)
         if user:
             flash('Email already exists.', category='error')
         elif len(email) < 4:
@@ -49,11 +94,21 @@ def signup():
             flash('Password must be at least 7 characters.', category='error')
         else:
             # Create new user in Firebase
-            new_user = time_capsule_firebase_obj.create_user(email, password1)
-            print(new_user)
+            new_user = time_capsule_firebase_admin_obj.create_user(email, password1)
             flash('Account created!', category='success')
             return redirect(url_for('home'))
+    
+    if 'logged_in' in session:
+            if session['logged_in']:
+                return redirect(url_for('home'))
+
     return render_template('signup.html')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.clear()
+    time_capsule_pyrebase_obj.sign_user_out()
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
---
+++ src/flask/utils/pyrebase.py
@@ -0,0 +1,21 @@
+# This is all pseudo code
+import pyrebase
+from firebase_config import config
+import ipdb
+'''
+This is a Pyrebase Obj that we create that wraps all our firebase related use cases
+Sign user in
+Sign user out
+'''
+class TimeCapsulePyrebaseObj:
+    def __init__(self):
+        self.pyrebase = pyrebase.initialize_app(config)
+        self.auth = self.pyrebase.auth()
+    
+    def sign_user_in(self, email, password):
+        response = self.auth.sign_in_with_email_and_password(email,password)
+        return response
+    
+    def sign_user_out(self):
+        self.auth.current_user = None
+        return
