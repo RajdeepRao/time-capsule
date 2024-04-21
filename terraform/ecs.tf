277fb1cdb8ae2422590b1310a39c5c10574fec40
Rajdeep Rao <rajdeep.rao@datadoghq.com>
2024-04-21 17:06:01 -0400

    WIP - Break through errors and deploy service

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,3 +1,14 @@
+data "aws_vpc" "default" {
+  default = true
+} 
+
+data "aws_subnets" "time_capsule_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 resource "aws_cloudwatch_log_group" "time_capsule" {
   name = "time-capsule-tf-log-group"
   tags = {
@@ -20,34 +31,63 @@ resource "aws_ecs_cluster" "time_capsule" {
     }
   }
 }
+resource "aws_security_group" "allow_tls_to_ecs" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.default.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_full_to_ecs_ipv4" {
+  security_group_id = aws_security_group.allow_tls_to_ecs.id
+  cidr_ipv4         = "162.84.168.225/32"
+  from_port         = 0
+  ip_protocol       = -1
+  to_port           = 0
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_full_to_ecs_ipv4_from_home" {
+  security_group_id = aws_security_group.allow_tls_to_ecs.id
+  cidr_ipv4         = "68.237.105.18/32"
+  from_port         = 0
+  ip_protocol       = -1
+  to_port           = 0
+}
+resource "aws_vpc_security_group_egress_rule" "allow_tls_to_ecs_ipv4" {
+  security_group_id = aws_security_group.allow_tls_to_ecs.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
 
 resource "aws_ecs_service" "time_capsule" {
   name            = "time-capsule-tf-service"
   cluster         = aws_ecs_cluster.time_capsule.id
-  task_definition = aws_ecs_task_definition.mongo.arn
+  task_definition = aws_ecs_task_definition.time_capsule.arn
   desired_count   = 1
-  iam_role        = aws_iam_role.foo.arn
-  depends_on      = [aws_iam_role_policy.foo]
+  # iam_role        = aws_iam_role.time_capsule_ecs_task_role.arn
+  launch_type    = "FARGATE"
 
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.time_capsule.arn
+  #   container_name   = "time-capsule-tf-container"
+  #   container_port   = 8080
+  # }
+  network_configuration {
+    subnets          = data.aws_subnets.time_capsule_subnets.ids
+    security_groups  = [aws_security_group.allow_tls_to_ecs.id]
+    assign_public_ip = true
   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.foo.arn
-    container_name   = "mongo"
-    container_port   = 8080
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
 }
 
-resource "aws_iam_role" "foo" {
-  name = "time-capsule-tf-role"
+data "aws_iam_policy" "AmazonECSTaskExecutionRolePolicy" {
+  name = "AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "time_capsule_ecs_task_role" {
+  name = "time-capsule-ecs-task-role-tf"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -55,7 +95,7 @@ resource "aws_iam_role" "foo" {
     {
       "Effect": "Allow",
       "Principal": {
-        "Service": "ecs.amazonaws.com"
+        "Service": "ecs-tasks.amazonaws.com"
       },
       "Action": "sts:AssumeRole"
     }
@@ -67,32 +107,47 @@ EOF
     Environment = "production"
     Application = "Time Capsule"
   }
+  managed_policy_arns = [ data.aws_iam_policy.AmazonECSTaskExecutionRolePolicy.arn ]
 }
-resource "aws_ecs_task_definition" "mongo" {
-  family                   = "time-capsule-tf-task"
-  execution_role_arn       = aws_iam_role.foo.arn
-  task_role_arn            = aws_iam_role.foo.arn
+
+resource "aws_ecs_task_definition" "time_capsule" {
+  family                   = "time-capsule-tf-td"
+  execution_role_arn       = aws_iam_role.time_capsule_ecs_task_role.arn
+  task_role_arn            = aws_iam_role.time_capsule_ecs_task_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  cpu = 1024
+  memory = 3072
 
   container_definitions = <<EOF
 [
   {
-    "name": "mongo",
-    "image": "mongo:latest",
+    "name": "time-capsule-tf-container",
+    "image": "926846101699.dkr.ecr.us-east-1.amazonaws.com/time-capsule-manual:latest",
+    "cpu": 0,
     "portMappings": [
-      {
-        "containerPort": 27017,
-        "protocol": "tcp"
-      }
+        {
+            "name": "time-capsule-manual-container-80-tcp",
+            "containerPort": 80,
+            "hostPort": 80,
+            "protocol": "tcp",
+            "appProtocol": "http"
+        }
     ],
+    "essential": true,
+    "environment": [],
+    "environmentFiles": [],
+    "mountPoints": [],
+    "volumesFrom": [],
+    "ulimits": [],
     "logConfiguration": {
-      "logDriver": "awslogs",
-      "options": {
-        "awslogs-group": "${aws_cloudwatch_log_group.time_capsule.name}",
-        "awslogs-region": "us-east-1",
-        "awslogs-stream-prefix": "mongo"
-      }
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "${aws_cloudwatch_log_group.time_capsule.name}",
+          "awslogs-region": "us-east-1",
+          "awslogs-stream-prefix": "ecs"
+        },
+        "secretOptions": []
     }
   }
 ]
